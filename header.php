26061cb6be4398fd457cf964d6b65a92088c25ae
vlad706 <vlad@athemes.com>
2018-05-09 14:29:25 +0300

    Site branding
    Site title fix, site description display

--- header.php
+++ header.php
@@ -107,7 +107,16 @@
 		</div>
 		<div class="site-branding site-branding--desktop mo-hide-on-mobile">
 			<?php the_custom_logo(); ?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php endif; ?>
+			<?php $cali_description = get_bloginfo( 'description', 'display' );
+			if ( $cali_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $cali_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
 		</div><!-- .site-branding--desktop -->
 	</header><!-- #masthead -->
 
